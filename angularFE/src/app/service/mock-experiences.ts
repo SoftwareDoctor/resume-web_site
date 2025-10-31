3cfbfef68f9898ff463106c2a0ab56beae6a897a
Andrea Italiano <andrea.italiano@senseisrl.it>
2025-10-31 09:50:22 +0100

    fix

--- angularFE/src/app/service/mock-experiences.ts
+++ angularFE/src/app/service/mock-experiences.ts
@@ -9,9 +9,9 @@ export const MOCK_EXPERIENCES: Experience[] = [
   },
   {
     id: 2,
-    company: "",
-    role: "Generali",
-    period: ""
+    company: "Generali",
+    role: "Analista funzionale",
+    period: "09/2022 - 03/2023"
   },
   {
      id: 3,
@@ -28,7 +28,7 @@ export const MOCK_EXPERIENCES: Experience[] = [
   {
     id: 5,
     company: "ANT SRL",
-    role: "",
+    role: "HR Specialist - Quality Assurance Specialist - IT Recruiter",
     period: "05/2016 - 08/2021"
   }
 ];
