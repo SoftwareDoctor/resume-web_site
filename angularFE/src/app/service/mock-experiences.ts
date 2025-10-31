304599acf589d383167a4962440765571e697f19
Andrea Italiano <andrea.italiano@senseisrl.it>
2025-10-31 08:41:47 +0100

    fix

--- angularFE/src/app/service/mock-experiences.ts
+++ angularFE/src/app/service/mock-experiences.ts
@@ -6,7 +6,6 @@ export const MOCK_EXPERIENCES: Experience[] = [
         company: "Sensei srl",
         role: "Junior Backend Java",
         period: "09/2024 - oggi"
-
   },
   {
     id: 2,
@@ -19,20 +18,17 @@ export const MOCK_EXPERIENCES: Experience[] = [
         company: "MediaEngine",
         role: "Test Analyst",
         period: "03/2022 - 09/2022"
-
   },
   {
      id: 4,
         company: "ANT SRL",
         role: "Analista Funzionale & Software Tester",
         period: "09/2021 - 03/2022"
-
   },
   {
     id: 5,
     company: "ANT SRL",
     role: "",
     period: "05/2016 - 08/2021"
-
   }
 ];
